6edec21e2ecb0a33f6fd1288f2481d3454605409
KrzysztofMadejski <krzysztof@madejscy.pl>
2015-04-04 11:36:41 +0200

    closes https://github.com/DanePubliczneGovPl/ckanext-danepubliczne/issues/135

--- ckan/public/base/i18n/pl.js
+++ ckan/public/base/i18n/pl.js
@@ -4,28 +4,116 @@
     "lang": "pl",
     "plural-forms": "nplurals=3; plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);"
   },
+  "An Error Occurred": [
+    null,
+    "Wystąpił błąd"
+  ],
+  "Are you sure you want to perform this action?": [
+    null,
+    "Czy na pewno chcesz wykonać tę akcję?"
+  ],
   "Cancel": [
     null,
     "Anuluj"
   ],
+  "Confirm": [
+    null,
+    "Zatwierdź"
+  ],
   "Edit": [
     null,
-    "Edycja"
+    "Edytuj"
+  ],
+  "Failed to load data API information": [
+    null,
+    "Nie udało się pobrać informacji o dostępie poprzez API"
+  ],
+  "Follow": [
+    null,
+    "Obserwuj"
+  ],
+  "Hide": [
+    null,
+    "Ukryj"
+  ],
+  "Image": [
+    null,
+    "Obraz"
+  ],
+  "Input is too short, must be at least one character": [
+    null,
+    "Wpisz przynajmniej jeden znak"
   ],
   "Loading...": [
     null,
-    "Ładowanie..."
+    "Trwa wczytywanie..."
+  ],
+  "No matches found": [
+    null,
+    "Nie znaleziono wyników"
+  ],
+  "Please Confirm Action": [
+    null,
+    "Potwierdź wykonanie akcji"
+  ],
+  "Remove": [
+    null,
+    "Usuń"
+  ],
+  "Resource uploaded": [
+    null,
+    "Przesłano zasób"
+  ],
+  "Show more": [
+    null,
+    "Pokaż więcej"
+  ],
+  "Start typing…": [
+    null,
+    "Zacznij wpisywać…"
+  ],
+  "There are unsaved modifications to this form": [
+    null,
+    "Wartości formularza są niezapisane"
+  ],
+  "Brak danych o API dla tego zasobu": [
+    null,
+    "There is no API data to load for this resource"
   ],
   "URL": [
     null,
     "URL"
   ],
+  "Unable to authenticate upload": [
+    null,
+    "Brak praw do przesłania zasobu"
+  ],
+  "Unable to get data for uploaded file": [
+    null,
+    "Nie można pobrać danych przesłanego pliku"
+  ],
+  "Unable to upload file": [
+    null,
+    "Nie można przesłać pliku"
+  ],
+  "Unfollow": [
+    null,
+    "Przestań obserwować"
+  ],
   "Upload": [
     null,
-    "Prześlij plik"
+    "Prześlij"
   ],
   "Upload a file": [
     null,
     "Prześlij plik"
+  ],
+  "show less": [
+    null,
+    "pokaż mniej"
+  ],
+  "show more": [
+    null,
+    "pokaż więcej"
   ]
 }
