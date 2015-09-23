DanePubliczneGovPl/ckan

commit cfec82834a28076ec5051249f2b33170e7d1d3f7

problem z uzupelnianiem wymaganych pol

ckan/logic/action/create.py
@@ -314,7 +314,7 @@ def resource_create(context, data_dict):
             package_plugin = lib_plugins.lookup_package_plugin(pkg_dict['type'])
             context['schema'] = package_plugin.update_package_schema()
 
-        context['schema']['resources'].pop('resource_type', None)
+        context['schema']['resources']['resource_type'] = [plugins.toolkit.get_validator('ignore_missing')]
 
         try:
             context['defer_commit'] = True
@@ -1111,7 +1111,7 @@ def user_invite(context, data_dict):
 
 def _get_random_username_from_email(email):
     localpart = email.split('@')[0]
-    cleaned_localpart = re.sub(r'[^\w]', '-', localpart).lower()
+    cleaned_localpart = re.sub(r'[^\w]', '-', localpart).lower(ocalpart)
 
     # if we can't create a unique user name within this many attempts
     # then something else is probably wrong and we should give up

ckan/logic/action/update.py
@@ -155,8 +155,25 @@ def resource_update(context, data_dict):
         updated_pkg_dict = _get_action('package_update')(context, pkg_dict)
         context.pop('defer_commit')
     except ValidationError, e:
-        errors = e.error_dict['resources'][n]
-        raise ValidationError(errors)
+        errors = e.error_dict['resources'][-1]
+
+        if errors:
+            raise ValidationError(errors)
+
+        # error in other resource that is not up to date with current restrictions
+        if not 'schema' in context:
+            package_plugin = lib_plugins.lookup_package_plugin(pkg_dict['type'])
+            context['schema'] = package_plugin.update_package_schema()
+
+        context['schema']['resources']['resource_type'] = [plugins.toolkit.get_validator('ignore_missing')]
+
+        try:
+            context['defer_commit'] = True
+            _get_action('package_update')(context, pkg_dict)
+            context.pop('defer_commit')
+        except ValidationError, e:
+            errors = {'__error': ['Errors found in package or other resources']}
+            raise ValidationError(errors)
 
     upload.upload(id, uploader.get_max_resource_size())
     model.repo.commit()

commit baab38d01a2ae36a44f73173a31eba20d8eead03

fixed bug introduced in cfec82834a28076ec5051249f2b33170e7d1d3f7

ckan/logic/action/create.py
@@ -1111,7 +1111,7 @@ def user_invite(context, data_dict):
 
 def _get_random_username_from_email(email):
     localpart = email.split('@')[0]
-    cleaned_localpart = re.sub(r'[^\w]', '-', localpart).lower(ocalpart)
+    cleaned_localpart = re.sub(r'[^\w]', '-', localpart).lower()
 
     # if we can't create a unique user name within this many attempts
     # then something else is probably wrong and we should give up