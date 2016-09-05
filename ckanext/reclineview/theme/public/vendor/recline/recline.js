5fab074b2da743d547692991f711ef03086f3682
krzysztofmadejski <krzysztof@madejscy.pl>
2016-09-05 11:22:56 +0200

    Mapy od OpenStreetMap.Mapnik, closes https://github.com/DanePubliczneGovPl/ckanext-danepubliczne/issues/241

--- ckanext/reclineview/theme/public/vendor/recline/recline.js
+++ ckanext/reclineview/theme/public/vendor/recline/recline.js
@@ -2283,9 +2283,9 @@ my.Map = Backbone.View.extend({
     var self = this;
     this.map = new L.Map(this.$map.get(0));
 
-    var mapUrl = "//otile{s}-s.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png";
-    var osmAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="//developer.mapquest.com/content/osm/mq_logo.png">';
-    var bg = new L.TileLayer(mapUrl, {maxZoom: 18, attribution: osmAttribution ,subdomains: '1234'});
+    var mapUrl = "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    var osmAttribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+    var bg = new L.TileLayer(mapUrl, {maxZoom: 18, attribution: osmAttribution ,subdomains: 'abc'});
     this.map.addLayer(bg);
 
     this.markers = new L.MarkerClusterGroup(this._clusterOptions);
